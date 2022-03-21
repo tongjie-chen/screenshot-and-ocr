9ee06f1f99f0e882c9183de959870efec844aaa2
Tongjie Chen <66334428+tongjie-chen@users.noreply.github.com>
2022-03-21 17:10:26 -0400

    Corrected gnome-screenshot options

--- ocr.sh
+++ ocr.sh
@@ -1,5 +1,5 @@
 xfce4-screenshooter -r --save /tmp/ocr-tmp.png
-# gnome-screenshot -a --save /tmp/ocr-tmp.png
+# gnome-screenshot -a -f /tmp/ocr-tmp.png
 tesseract -l eng /tmp/ocr-tmp.png /tmp/ocr-output
 cat /tmp/ocr-output.txt | tr "\n" " " | xclip -in -selection clipboard
 rm /tmp/ocr-tmp.png /tmp/ocr-output.txt
